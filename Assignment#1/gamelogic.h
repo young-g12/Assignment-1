e9264882ba89fe368fd3f24345eafa5a432a888b
Gil <gmiranda540@yahoo.com>
2026-05-22 15:19:46 -0400

    testing initial gamestate

---
+++ Assignment#1/gamelogic.h
@@ -0,0 +1,8 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+
+class logic {
+
+};
+
+#endif
---
+++ Assignment#1/main.cpp
@@ -0,0 +1,13 @@
+// Assignment#1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "starting game" << endl;
+
+    return 0;
+}
